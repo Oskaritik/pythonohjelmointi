743ee3a518baf033f235487a0753ffedb7841700
oskar <oskari.tikkala05@gmail.com>
2026-02-25 10:03:47 +0200

    hello

---
+++ Moduuli 2/tehtava 3,1.py
@@ -0,0 +1,5 @@
+kuha= float(input("anna kuhan pituus:"))
+
+if kuha < 37
+    erotus=37-kuha
+    print("Laske kuha takaisin järveen"-kuha)
--- Moduuli 2/tehtava 4.2.py
+++ Moduuli 2/tehtava 4.2.py
@@ -1,10 +1,10 @@
 while True:
-    tuumat = float(input("anna tuuma määrä (negatiivinen luku lopettaa):"))
+    tuumat = float(input("anna tuuma määrä (negatiivinen luku lopettaa):" ))
 
     if tuumat < 0:
-        print("ohjelma lopetataan")
+        print( "ohjelma lopetataan" )
         break
 
-    senttimetrit= tuumat * 2.54
+    senttimetrit = tuumat * 2.54
 
-    print(f"{tuumat} tuuma = {senttimetrit:.2f} cm")
+    print(f" {tuumat} tuuma = {senttimetrit:.2f} cm")
